9fde44985e8d6a14dc6c6437870f45f4c8ae3e99
st-tranle <tran.le@supremetech.vn>
2025-06-02 14:49:18 +0700

    refactor: change name of parameter in signIn function

--- app/src/main/java/com/android/internship/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/android/internship/domain/repository/AuthRepository.kt
@@ -3,6 +3,6 @@ package com.android.internship.domain.repository
 import com.android.internship.data.model.SignInResponse
 
 interface AuthRepository {
-    suspend fun signIn(username: String, password: String): SignInResponse
+    suspend fun signIn(email: String, password: String): SignInResponse
     fun isSignedIn(): Boolean?
 }
