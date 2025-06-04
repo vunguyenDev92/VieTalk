94407e4e0fbb34f20a56435764a659612d167203
st-vunguyen3 <vu.nguyen3@supremetech.vn>
2025-06-04 10:26:59 +0700

    fix: fix miss logic

--- app/src/main/java/com/android/internship/presentation/screens/SpashScreen.kt
+++ app/src/main/java/com/android/internship/presentation/screens/SpashScreen.kt
@@ -1,26 +1,21 @@
 package com.android.internship.presentation.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.android.internship.R
+import com.android.internship.di.AppContainer
+import com.android.internship.domain.usecase.GetSignInStatus
 import com.android.internship.presentation.navigation.Screen
-import com.android.internship.presentation.theme.Black
-import com.android.internship.presentation.theme.Black35
 import kotlinx.coroutines.delay
 
 @Composable
@@ -30,8 +25,14 @@ fun SplashScreen(
 ) {
     LaunchedEffect(Unit) {
         delay(1000)
-        navController.navigate(Screen.SignIn) {
-            popUpTo(navController.graph.startDestinationId) { inclusive = true }
+        if (GetSignInStatus(AppContainer(navController.context).authRepository).invoke() == true) {
+            navController.navigate(Screen.Chat) {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+            }
+        } else {
+            navController.navigate(Screen.SignIn) {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+            }
         }
     }
 
