1b52b455bdabca20cd7f92f881d6f680c9e1af56
st-vunguyen3 <vu.nguyen3@supremetech.vn>
2025-06-08 18:14:08 +0700

    fix: delete file in userdao

--- app/src/main/java/com/android/internship/data/datasource/local/dao/UserDao.kt
+++ app/src/main/java/com/android/internship/data/datasource/local/dao/UserDao.kt
@@ -1,14 +1,6 @@
 package com.android.internship.data.datasource.local.dao
 
 import androidx.room.Dao
-import androidx.room.Query
-import com.android.internship.data.datasource.local.entity.UserEntity
 
 @Dao
-interface UserDao {
-    @Query("SELECT * FROM users LIMIT 1")
-    suspend fun getCurrentUser(): UserEntity?
-
-    @Query("SELECT uid FROM users LIMIT 1")
-    suspend fun getCurrentUserId(): String?
-}
+interface UserDao
