ecdf49ae84a339395d92d586a08c3906b14a8bf1
Hiep Nguyen P. <hiep.nguyen@supremetech.vn>
2025-06-19 11:15:58 +0700

    fix: move bitmap loading for notifications to service and update NotificationHelper interface

--- app/src/main/java/com/android/internship/presentation/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/android/internship/presentation/utils/MyFirebaseMessagingService.kt
@@ -1,7 +1,16 @@
 package com.android.internship.presentation.utils
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import com.android.internship.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.net.URL
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -13,16 +22,29 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             val imageUri = remoteMessage.notification?.imageUrl
             val data = remoteMessage.data
 
-            NotificationHelper(this).showNotification(
-                title = title,
-                message = message,
-                imageUri = imageUri,
-                data = data,
-            )
+            CoroutineScope(Dispatchers.IO).launch {
+                val bitmap = loadBitmapFromUri(this@MyFirebaseMessagingService, imageUri)
+                NotificationHelper(this@MyFirebaseMessagingService).showNotification(
+                    title = title,
+                    message = message,
+                    bitmap = bitmap,
+                    data = data,
+                )
+            }
         }
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    private fun loadBitmapFromUri(context: Context, uri: Uri?): Bitmap? {
+        val inputStream = uri?.let {
+            URL(it.toString()).openStream()
+        }
+        if (inputStream == null) {
+            return BitmapFactory.decodeResource(context.resources, R.drawable.ic_person_color)
+        }
+        return inputStream.use { BitmapFactory.decodeStream(it) }
     }
 }
--- app/src/main/java/com/android/internship/presentation/utils/NotificationManager.kt
+++ app/src/main/java/com/android/internship/presentation/utils/NotificationManager.kt
@@ -5,13 +5,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.media.RingtoneManager
-import android.net.Uri
 import androidx.core.app.NotificationCompat
 import com.android.internship.R
 import com.android.internship.presentation.MainActivity
-import java.net.URL
 
 class NotificationHelper(private val context: Context) {
 
@@ -38,7 +35,7 @@ class NotificationHelper(private val context: Context) {
         notificationManager.createNotificationChannel(channel)
     }
 
-    fun showNotification(title: String?, message: String?, imageUri: Uri?, data: Map<String, String>?) {
+    fun showNotification(title: String?, message: String?, bitmap: Bitmap?, data: Map<String, String>?) {
         val pendingIntent = PendingIntent.getActivity(
             context,
             0,
@@ -55,19 +52,11 @@ class NotificationHelper(private val context: Context) {
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
-            .setLargeIcon(loadBitmapFromUri(context, imageUri))
+            .setLargeIcon(bitmap)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
 
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         notificationManager.notify(0, notificationBuilder.build())
-    }
-
-    private fun loadBitmapFromUri(context: Context, uri: Uri?): Bitmap? {
-        val inputStream = uri?.let { URL(it.toString()).openStream() }
-        if (inputStream == null) {
-            return BitmapFactory.decodeResource(context.resources, R.drawable.ic_person_color)
-        }
-        return inputStream.use { BitmapFactory.decodeStream(it) }
     }
 }
